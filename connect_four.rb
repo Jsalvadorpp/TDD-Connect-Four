5788a92a76d61d98f3e253666c2820fba4c640b8
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-08-25 10:44:41 -0400

    add winning condition

--- connect_four.rb
+++ connect_four.rb
@@ -38,13 +38,19 @@ class Game
         until gameOver
             displayBoard()
             playerTurn()
+            break if checkCombination("o")
             displayBoard()
             #PC_turn()
             puts "PC turn, press any key to continue"
             PC_Turn()
+            break if checkCombination("*")
             
         end
 
+        player = gameResults()
+        puts " *****************************"
+        puts " ***      #{player} WON    ***"
+        puts " *****************************"
 
 
 
@@ -84,8 +90,84 @@ class Game
 
     end
 
+    def checkCombination(piece)
+
+        #check  horizontal cells
+        Rows.times do |row|
+            horizontalCells = []
+            Columns.times { |cell| horizontalCells.push(@GameBoard[row][cell]) }
+            return true if checkCells(horizontalCells,piece)
+        end
+
+        #check vertical cells
+        Columns.times do |column|
+            columnCells = []
+            Rows.times { |cell| columnCells.push(@GameBoard[cell][column])}
+            return true if checkCells(columnCells,piece)
+        end
+
+        #check left diagonal cells 
+        Rows.times do |row|
+            diagonalLeftCells = []
+            column = 0
+            (row...Rows).each do |x|
+                diagonalLeftCells.push(@GameBoard[x][column])
+                column+=1
+            end
+            return true if checkCells(diagonalLeftCells,piece)
+        end
+
+        (1...Columns).each do |column|
+            diagonalLeftCells = []
+            row = 0
+            (column...Columns).each do |y|
+                diagonalLeftCells.push(@GameBoard[row][y])
+                row += 1
+            end
+            return true if checkCells(diagonalLeftCells,piece)
+        end
+
+        #check right diagonal cells
+        Rows.times do |row|
+            diagonalRightCells = []
+            column = Columns - 1
+            (row...Rows).each do |x|
+                diagonalRightCells.push(@GameBoard[x][column])
+                column-=1
+            end
+            return true if checkCells(diagonalRightCells,piece)
+        end
+
+        (Columns-2).downto(0) do |column|
+            diagonalRightCells = []
+            row = 0
+            column.downto(0) do |y|
+                diagonalRightCells.push(@GameBoard[row][y])
+                row += 1
+            end
+            return true if checkCells(diagonalRightCells,piece)
+        end
+      
+        return false
+    end
+
+    def checkCells(cellsList,piece)
+        return false unless cellsList.any?(piece)
+        count = 0
+        cellsList.each do |cell|
+            count = (cell == piece)? (count+1):0
+            return true if count == 4
+        end
+        return false
+    end
+
+    def gameResults
+        return "You" if checkCombination("o")
+        return "PC" if checkCombination("*")
+    end
+
 
 end
 
 game = Game.new
-game.displayBoard()
+
